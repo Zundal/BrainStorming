b37520e335c60dc496136dc59a3bda20cba4c21c
Zundal <ajs15010120@gmail.com>
2021-10-16 19:50:33 +0900

    가장긴 강소하는수

--- Jeongsik/week24/longestMin.py
+++ Jeongsik/week24/longestMin.py
@@ -1,13 +1,13 @@
 # 앞쪽의 숫자의 부분 수열의 길이에 1을 더한 값이 자신의 부분 수열
 
-cnt = int(input())
-inputNum = list(map(int, input().split()))
+N = int(input())
+numbers = list(map(int, input().split()))
 
-arr = [1] * cnt
+dp = [1] * N
 
-for i in range(1, cnt):
+for i in range(1, N):
     for j in range(i):
-        if inputNum[j] > inputNum[i]:
-            arr[i] = max(arr[i], arr[j] + 1)
+        if numbers[j] > numbers[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-print(max(arr))
+print(max(dp))
